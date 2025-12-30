94e23046aa57c26b855381e02d7272bb5ca3e1f4
Staili Saad <saadstaili1903@gmail.com>
2025-12-30 12:59:49 +0100

    Add Compose dependencies to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation "androidx.activity:activity-compose:1.8.2"
+    implementation "androidx.compose.ui:ui:1.5.4"
+    implementation "androidx.compose.material3:material3:1.2.0"
 }
 
